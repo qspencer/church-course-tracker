374156f90c094bc1c66c9f56345cdd9ce964156c
Quentin Spencer <qspencer@gmail.com>
2025-10-17 18:51:27 +0000

    fix: remove add_csv_source_tracking reference from merge migration
    - Update merge_migration_heads.py to remove reference to deleted migration
    - This should resolve the KeyError: 'add_csv_source_tracking' issue
    - Application should now start successfully

--- backend/migrations/versions/merge_migration_heads.py
+++ backend/migrations/versions/merge_migration_heads.py
@@ -1,7 +1,7 @@
 """Merge migration heads
 
 Revision ID: merge_heads_001
-Revises: 8a1b2c3d4e5f, 6e85db6cd84c, add_csv_source_tracking
+Revises: 8a1b2c3d4e5f, 6e85db6cd84c
 Create Date: 2025-10-13 18:30:00.000000
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'merge_heads_001'
-down_revision = ('8a1b2c3d4e5f', '6e85db6cd84c', 'add_csv_source_tracking')
+down_revision = ('8a1b2c3d4e5f', '6e85db6cd84c')
 branch_labels = None
 depends_on = None
 
