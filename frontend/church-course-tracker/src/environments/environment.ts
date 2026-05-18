82ec3fbe2a4120f3aed51aea6a622ecf74fd88d3
GitHub Action <action@github.com>
2026-05-18 19:48:04 +0000

    Auto-increment version to 0.77 [skip ci]

--- frontend/church-course-tracker/src/environments/environment.ts
+++ frontend/church-course-tracker/src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8000/api/v1',
   appName: 'Church Course Tracker',
-  version: '0.76', // This should match environment.prod.ts - will be replaced during build
+  version: '0.77', // This should match environment.prod.ts - will be replaced during build
   enableAnalytics: false,
   enableErrorReporting: false,  // Disabled in dev
   logLevel: 'debug', // 'debug', 'info', 'warn', 'error'
